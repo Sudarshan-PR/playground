c89fbf78165ef1f3b276984ffdc5b2030f90316c
Sudarshan P <psudarshan37@gmail.com>
2023-11-18 00:40:57 +0000

    feat: load environment vars from .env

--- gateway/models/models.go
+++ gateway/models/models.go
@@ -1,6 +1,7 @@
 package models
 
 type CompileBody struct {
+	ID string `json:"client_id"`
 	Language string `json:"language"`
 	Code string `json:"code"`
 }
--- gateway/setup/setup.go
+++ gateway/setup/setup.go
@@ -1,12 +1,20 @@
 package setup
 
 import (
+	"log"
 	"os"
 
 	"github.com/Sudarshan-PR/playground/gateway/models"
+	"github.com/joho/godotenv"
 )
 
 func Setup() error {
+	// Load env variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	mq, err := models.CreateRabbitMQConnection(os.Getenv("RABBITMQ_ADDRESS"))
 	if err != nil {
 		return err
